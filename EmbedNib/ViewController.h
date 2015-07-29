a5bdfb442e1b849fcefe0bb46e5a351a762278f2
fido0725 <chenfong_12@163.com>
2015-07-29 15:58:51 +0800

    Initial Commit

---
+++ EmbedNib/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EmbedNib
+//
+//  Created by chenfeng on 15/7/29.
+//  Copyright (c) 2015年 fido0725. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ EmbedNibTests/EmbedNibTests.m
@@ -0,0 +1,40 @@
+//
+//  EmbedNibTests.m
+//  EmbedNibTests
+//
+//  Created by chenfeng on 15/7/29.
+//  Copyright (c) 2015年 fido0725. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface EmbedNibTests : XCTestCase
+
+@end
+
+@implementation EmbedNibTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
